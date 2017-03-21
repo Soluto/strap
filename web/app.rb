7560a17a28bd07df57c85275acec436bb710f595
Or Yagel <or@soluto.com>
2017-03-21 10:24:10 +0200

    add text

--- web/app.rb
+++ web/app.rb
@@ -39,6 +39,12 @@ get "/" do
   @title = "Strap"
   @text = <<-EOS
 Strap is a script to bootstrap a minimal macOS development system. This does not assume you're doing Ruby/Rails/web development but installs the minimal set of software every macOS developer will want.
+</br></br>
+Dependencies:
+<ol>
+	<li>Github account with dotfiles repository. You can start by forking an <a href="https://github.com/oryagel/dotfiles">existing repository</a></li>
+	<li>Apple Id for the Mac App store</li>
+</ol>
 </br>
 What this script does:
 <ol>
